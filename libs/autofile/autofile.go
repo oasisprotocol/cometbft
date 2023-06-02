ptrus/tendermint

libs/autofile: Don't panic on EMFILE
The autofile group code is written so that it panics on errors when
rotating the file, which causes the node to crash if file descriptors
ever get exhausted (in particular due to running against the rlimit).

Handle this condition gracefully, by retrying periodically instead.

libs/autofile/autofile.go
@@ -8,6 +8,8 @@ import (
 	"time"
 
 	cmn "github.com/tendermint/tendermint/libs/common"
+
+	"github.com/oasislabs/safeopen"
 )
 
 /* AutoFile usage
@@ -37,6 +39,8 @@ const (
 	autoFilePerms       = os.FileMode(0600)
 )
 
+var defaultAutoOpener = safeopen.NewOpener()
+
 // AutoFile automatically closes and re-opens file for writing. The file is
 // automatically setup to close itself every 1s and upon receiving SIGHUP.
 //
@@ -51,6 +55,8 @@ type AutoFile struct {
 
 	mtx  sync.Mutex
 	file *os.File
+
+	opener *safeopen.Opener
 }
 
 // OpenAutoFile creates an AutoFile in the path (with random ID). If there is
@@ -62,6 +68,7 @@ func OpenAutoFile(path string) (*AutoFile, error) {
 		Path:             path,
 		closeTicker:      time.NewTicker(autoFileClosePeriod),
 		closeTickerStopc: make(chan struct{}),
+		opener:           defaultAutoOpener,
 	}
 	if err := af.openFile(); err != nil {
 		af.Close()
@@ -152,7 +159,7 @@ func (af *AutoFile) Sync() error {
 }
 
 func (af *AutoFile) openFile() error {
-	file, err := os.OpenFile(af.Path, os.O_RDWR|os.O_CREATE|os.O_APPEND, autoFilePerms)
+	file, err := af.opener.OpenFile(af.Path, os.O_RDWR|os.O_CREATE|os.O_APPEND, autoFilePerms)
 	if err != nil {
 		return err
 	}

libs/autofile/group.go
@@ -2,6 +2,7 @@ package autofile
 
 import (
 	"bufio"
+	"context"
 	"errors"
 	"fmt"
 	"io"
@@ -13,6 +14,8 @@ import (
 	"strings"
 	"sync"
 	"time"
+
+	"github.com/oasislabs/safeopen"
 
 	cmn "github.com/tendermint/tendermint/libs/common"
 )
@@ -74,6 +77,10 @@ type Group struct {
 
 	// TODO: When we start deleting files, we need to start tracking GroupReaders
 	// and their dependencies.
+
+	opener         *safeopen.Opener
+	openerCanceler context.CancelFunc
+	fdReclaimer    FDReclaimer
 }
 
 // OpenGroup creates a new Group with head at headPath. It returns an error if
@@ -85,6 +92,7 @@ func OpenGroup(headPath string, groupOptions ...func(*Group)) (g *Group, err err
 		return nil, err
 	}
 
+	ctx, cancelFn := context.WithCancel(context.Background())
 	g = &Group{
 		ID:                 "group:" + head.ID,
 		Head:               head,
@@ -96,11 +104,21 @@ func OpenGroup(headPath string, groupOptions ...func(*Group)) (g *Group, err err
 		minIndex:           0,
 		maxIndex:           0,
 		doneProcessTicks:   make(chan struct{}),
+		openerCanceler:     cancelFn,
 	}
 
 	for _, option := range groupOptions {
 		option(g)
 	}
+
+	g.opener = safeopen.NewOpener().WithContext(ctx).WithNotifier(func(error, time.Duration) {
+		if g.fdReclaimer != nil {
+			g.fdReclaimer.Reclaim()
+		}
+	})
+	g.mtx.Lock()
+	g.Head.opener = g.opener
+	g.mtx.Unlock()
 
 	g.BaseService = *cmn.NewBaseService(nil, "Group", g)
 
@@ -131,6 +149,13 @@ func GroupTotalSizeLimit(limit int64) func(*Group) {
 	}
 }
 
+// GroupFDReclaimer allows you to overwrite the fd reclamation hook.
+func GroupFDReclaimer(r FDReclaimer) func(*Group) {
+	return func(g *Group) {
+		g.fdReclaimer = r
+	}
+}
+
 // OnStart implements cmn.Service by starting the goroutine that checks file
 // and group limits.
 func (g *Group) OnStart() error {
@@ -142,6 +167,7 @@ func (g *Group) OnStart() error {
 // OnStop implements cmn.Service by stopping the goroutine described above.
 // NOTE: g.Head must be closed separately using Close.
 func (g *Group) OnStop() {
+	g.openerCanceler()
 	g.ticker.Stop()
 	g.FlushAndSync()
 }
@@ -354,7 +380,7 @@ func (g *Group) readGroupInfo() GroupInfo {
 	var minIndex, maxIndex int = -1, -1
 	var totalSize, headSize int64 = 0, 0
 
-	dir, err := os.Open(groupDir)
+	dir, err := g.opener.Open(groupDir)
 	if err != nil {
 		panic(err)
 	}
@@ -501,7 +527,7 @@ func (gr *GroupReader) openFile(index int) error {
 	}
 
 	curFilePath := filePathForIndex(gr.Head.Path, index, gr.Group.maxIndex)
-	curFile, err := os.OpenFile(curFilePath, os.O_RDONLY|os.O_CREATE, autoFilePerms)
+	curFile, err := gr.opener.OpenFile(curFilePath, os.O_RDONLY|os.O_CREATE, autoFilePerms)
 	if err != nil {
 		return err
 	}
@@ -531,4 +557,10 @@ func (gr *GroupReader) SetIndex(index int) error {
 	gr.mtx.Lock()
 	defer gr.mtx.Unlock()
 	return gr.openFile(index)
+}
+
+// FDReclaimer is the interface for a generic "reclaim file descriptors"
+// routine that will be called when the system exhausts available fds.
+type FDReclaimer interface {
+	Reclaim()
 }

oasisprotocol/cometbft

libs/autofile/autofile.go
@@ -9,6 +9,8 @@ import (
 	"time"
 
 	cmtrand "github.com/cometbft/cometbft/libs/rand"
+
+	"github.com/oasisprotocol/safeopen"
 )
 
 /* AutoFile usage
@@ -38,6 +40,8 @@ const (
 	autoFilePerms       = os.FileMode(0600)
 )
 
+var defaultAutoOpener = safeopen.NewOpener()
+
 // AutoFile automatically closes and re-opens file for writing. The file is
 // automatically setup to close itself every 1s and upon receiving SIGHUP.
 //
@@ -52,6 +56,8 @@ type AutoFile struct {
 
 	mtx  sync.Mutex
 	file *os.File
+
+	opener *safeopen.Opener
 }
 
 // OpenAutoFile creates an AutoFile in the path (with random ID). If there is
@@ -68,6 +74,7 @@ func OpenAutoFile(path string) (*AutoFile, error) {
 		Path:             path,
 		closeTicker:      time.NewTicker(autoFileClosePeriod),
 		closeTickerStopc: make(chan struct{}),
+		opener:           defaultAutoOpener,
 	}
 	if err := af.openFile(); err != nil {
 		af.Close()
@@ -158,7 +165,7 @@ func (af *AutoFile) Sync() error {
 }
 
 func (af *AutoFile) openFile() error {
-	file, err := os.OpenFile(af.Path, os.O_RDWR|os.O_CREATE|os.O_APPEND, autoFilePerms)
+	file, err := af.opener.OpenFile(af.Path, os.O_RDWR|os.O_CREATE|os.O_APPEND, autoFilePerms)
 	if err != nil {
 		return err
 	}

libs/autofile/group.go
@@ -2,6 +2,7 @@ package autofile
 
 import (
 	"bufio"
+	"context"
 	"errors"
 	"fmt"
 	"io"
@@ -12,6 +13,8 @@ import (
 	"strings"
 	"sync"
 	"time"
+
+	"github.com/oasisprotocol/safeopen"
 
 	"github.com/cometbft/cometbft/libs/service"
 )
@@ -73,6 +76,10 @@ type Group struct {
 
 	// TODO: When we start deleting files, we need to start tracking GroupReaders
 	// and their dependencies.
+
+	opener         *safeopen.Opener
+	openerCanceler context.CancelFunc
+	fdReclaimer    FDReclaimer
 }
 
 // OpenGroup creates a new Group with head at headPath. It returns an error if
@@ -87,6 +94,7 @@ func OpenGroup(headPath string, groupOptions ...func(*Group)) (*Group, error) {
 		return nil, err
 	}
 
+	ctx, cancelFn := context.WithCancel(context.Background())
 	g := &Group{
 		ID:                 "group:" + head.ID,
 		Head:               head,
@@ -98,11 +106,21 @@ func OpenGroup(headPath string, groupOptions ...func(*Group)) (*Group, error) {
 		minIndex:           0,
 		maxIndex:           0,
 		doneProcessTicks:   make(chan struct{}),
+		openerCanceler:     cancelFn,
 	}
 
 	for _, option := range groupOptions {
 		option(g)
 	}
+
+	g.opener = safeopen.NewOpener().WithContext(ctx).WithNotifier(func(error, time.Duration) {
+		if g.fdReclaimer != nil {
+			g.fdReclaimer.Reclaim()
+		}
+	})
+	g.mtx.Lock()
+	g.Head.opener = g.opener
+	g.mtx.Unlock()
 
 	g.BaseService = *service.NewBaseService(nil, "Group", g)
 
@@ -133,6 +151,13 @@ func GroupTotalSizeLimit(limit int64) func(*Group) {
 	}
 }
 
+// GroupFDReclaimer allows you to overwrite the fd reclamation hook.
+func GroupFDReclaimer(r FDReclaimer) func(*Group) {
+	return func(g *Group) {
+		g.fdReclaimer = r
+	}
+}
+
 // OnStart implements service.Service by starting the goroutine that checks file
 // and group limits.
 func (g *Group) OnStart() error {
@@ -144,6 +169,7 @@ func (g *Group) OnStart() error {
 // OnStop implements service.Service by stopping the goroutine described above.
 // NOTE: g.Head must be closed separately using Close.
 func (g *Group) OnStop() {
+	g.openerCanceler()
 	g.ticker.Stop()
 	if err := g.FlushAndSync(); err != nil {
 		g.Logger.Error("Error flushin to disk", "err", err)
@@ -360,7 +386,7 @@ func (g *Group) readGroupInfo() GroupInfo {
 	var minIndex, maxIndex int = -1, -1
 	var totalSize, headSize int64 = 0, 0
 
-	dir, err := os.Open(groupDir)
+	dir, err := g.opener.Open(groupDir)
 	if err != nil {
 		panic(err)
 	}
@@ -507,7 +533,7 @@ func (gr *GroupReader) openFile(index int) error {
 	}
 
 	curFilePath := filePathForIndex(gr.Head.Path, index, gr.Group.maxIndex)
-	curFile, err := os.OpenFile(curFilePath, os.O_RDONLY|os.O_CREATE, autoFilePerms)
+	curFile, err := gr.opener.OpenFile(curFilePath, os.O_RDONLY|os.O_CREATE, autoFilePerms)
 	if err != nil {
 		return err
 	}
@@ -537,4 +563,10 @@ func (gr *GroupReader) SetIndex(index int) error {
 	gr.mtx.Lock()
 	defer gr.mtx.Unlock()
 	return gr.openFile(index)
+}
+
+// FDReclaimer is the interface for a generic "reclaim file descriptors"
+// routine that will be called when the system exhausts available fds.
+type FDReclaimer interface {
+	Reclaim()
 }