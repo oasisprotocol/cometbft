ptrus/tendermint

crypto/ed25519: Use an optimized implementation
This switches the ed25519 import to use an optimzied ed25519
implementation to improve signing, verification, and key generation
performance.

Notes:

 * Go prior to 1.13 does not guarantee that certain `math/bits`
   intrinsics are constant time.  When building non-amd64 targets
   with older compilers, the 32 bit codepath is forced.

 * The 64 bit code path is only enabled for the amd64, arm64, ppc64
   and ppc64le targets due to the Go compiler's support for optimizing
   the `math.bits` intrinsics used.

 * Key generation and signing performance on targets that are not
   amd64, 386, and ppc64le will be slower than it could be due to
   the use of `subtle.ConstantTimeCopy` to implement a constant
   time table lookup.  Patches welcome.

crypto/ed25519/ed25519.go
@@ -6,8 +6,8 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/oasislabs/ed25519"
 	amino "github.com/tendermint/go-amino"
-	"golang.org/x/crypto/ed25519"
 
 	"github.com/tendermint/tendermint/crypto"
 	"github.com/tendermint/tendermint/crypto/tmhash"

oasisprotocol/cometbft

crypto/ed25519/ed25519.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"io"
 
-	"golang.org/x/crypto/ed25519"
+	"github.com/oasisprotocol/ed25519"
 
 	"github.com/cometbft/cometbft/crypto"
 	"github.com/cometbft/cometbft/crypto/tmhash"