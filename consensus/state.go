ptrus/tendermint

node: Flush pending incoming connections on EMFILE
If the consensus state WAL encounters EMFILE, flush pending incoming
connections to try to free up file descriptors.

consensus/state.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/pkg/errors"
 
+	auto "github.com/tendermint/tendermint/libs/autofile"
 	cmn "github.com/tendermint/tendermint/libs/common"
 	"github.com/tendermint/tendermint/libs/fail"
 	"github.com/tendermint/tendermint/libs/log"
@@ -133,6 +134,9 @@ type ConsensusState struct {
 
 	// for reporting metrics
 	metrics *Metrics
+
+	// for trying to recover faster on EMFILE.
+	fdReclaimer auto.FDReclaimer
 }
 
 // StateOption sets an optional parameter on the ConsensusState.
@@ -194,6 +198,11 @@ func (cs *ConsensusState) SetLogger(l log.Logger) {
 func (cs *ConsensusState) SetEventBus(b *types.EventBus) {
 	cs.eventBus = b
 	cs.blockExec.SetEventBus(b)
+}
+
+// SetFDReclaimer sets the fd reclaimer.
+func (cs *ConsensusState) SetFDReclaimer(r auto.FDReclaimer) {
+	cs.fdReclaimer = r
 }
 
 // StateMetrics sets the metrics.
@@ -367,7 +376,7 @@ func (cs *ConsensusState) Wait() {
 
 // OpenWAL opens a file to log all consensus messages and timeouts for deterministic accountability
 func (cs *ConsensusState) OpenWAL(walFile string) (WAL, error) {
-	wal, err := NewWAL(walFile)
+	wal, err := NewWAL(walFile, auto.GroupFDReclaimer(cs.fdReclaimer))
 	if err != nil {
 		cs.Logger.Error("Failed to open WAL for consensus state", "wal", walFile, "err", err)
 		return nil, err

oasisprotocol/cometbft

consensus/state.go
@@ -10,6 +10,7 @@ import (
 	"sort"
 	"time"
 
+	auto "github.com/cometbft/cometbft/libs/autofile"
 	"github.com/cosmos/gogoproto/proto"
 
 	cfg "github.com/cometbft/cometbft/config"
@@ -141,6 +142,9 @@ type State struct {
 
 	// for reporting metrics
 	metrics *Metrics
+
+	// for trying to recover faster on EMFILE.
+	fdReclaimer auto.FDReclaimer
 }
 
 // StateOption sets an optional parameter on the State.
@@ -209,6 +213,11 @@ func (cs *State) SetLogger(l log.Logger) {
 func (cs *State) SetEventBus(b *types.EventBus) {
 	cs.eventBus = b
 	cs.blockExec.SetEventBus(b)
+}
+
+// SetFDReclaimer sets the fd reclaimer.
+func (cs *State) SetFDReclaimer(r auto.FDReclaimer) {
+	cs.fdReclaimer = r
 }
 
 // StateMetrics sets the metrics.
@@ -436,7 +445,7 @@ func (cs *State) Wait() {
 // OpenWAL opens a file to log all consensus messages and timeouts for
 // deterministic accountability.
 func (cs *State) OpenWAL(walFile string) (WAL, error) {
-	wal, err := NewWAL(walFile)
+	wal, err := NewWAL(walFile, auto.GroupFDReclaimer(cs.fdReclaimer))
 	if err != nil {
 		cs.Logger.Error("failed to open WAL", "file", walFile, "err", err)
 		return nil, err