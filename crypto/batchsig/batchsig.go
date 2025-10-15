oasisprotocol/tendermint

types: Use Ed25519 batch verification in VerifyCommit

crypto/batchsig/batchsig.go
@@ -1,0 +1,44 @@
+// Package batchsig provides an abstraction for batch verifying public
+// key signatures.
+package batchsig
+
+import (
+	"fmt"
+
+	"github.com/oasisprotocol/ed25519"
+
+	"github.com/tendermint/tendermint/crypto"
+	tmEd25519 "github.com/tendermint/tendermint/crypto/ed25519"
+)
+
+var defaultOptions ed25519.Options
+
+// VerifyBatch verifies signatures in bulk.  Note that this call is only
+// faster than calling VerifyBytes for each signature iff every signature
+// is valid.
+func VerifyBatch(pubKeys []crypto.PubKey, msgs, sigs [][]byte) ([]bool, error) {
+	if len(pubKeys) != len(msgs) || len(msgs) != len(sigs) {
+		return nil, fmt.Errorf("tendermint/crypto/ed25519: parameter size mismatch")
+	}
+
+	// Currently only Ed25519 supports batch verification.
+	nativePubKeys := make([]ed25519.PublicKey, 0, len(pubKeys))
+	for i := range pubKeys {
+		edPubKey, ok := pubKeys[i].(tmEd25519.PubKey)
+		if !ok {
+			return verifyBatchFallback(pubKeys, msgs, sigs)
+		}
+
+		nativePubKeys = append(nativePubKeys, ed25519.PublicKey(edPubKey[:]))
+	}
+	_, validSigs, err := ed25519.VerifyBatch(nil, nativePubKeys, msgs, sigs, &defaultOptions)
+	return validSigs, err
+}
+
+func verifyBatchFallback(pubKeys []crypto.PubKey, msgs, sigs [][]byte) ([]bool, error) {
+	validSigs := make([]bool, len(pubKeys))
+	for i := range pubKeys {
+		validSigs[i] = pubKeys[i].VerifyBytes(msgs[i], sigs[i])
+	}
+	return validSigs, nil
+}

types/validator_set.go
@@ -9,6 +9,8 @@ import (
 	"sort"
 	"strings"
 
+	"github.com/tendermint/tendermint/crypto"
+	"github.com/tendermint/tendermint/crypto/batchsig"
 	"github.com/tendermint/tendermint/crypto/merkle"
 	tmmath "github.com/tendermint/tendermint/libs/math"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
@@ -680,6 +682,11 @@ func (vals *ValidatorSet) VerifyCommit(chainID string, blockID BlockID,
 
 	talliedVotingPower := int64(0)
 	votingPowerNeeded := vals.TotalVotingPower() * 2 / 3
+
+	// Aggregate all of the validator public key/messages/signatures.
+	validatorPublicKeys := make([]crypto.PubKey, 0, len(commit.Signatures))
+	commitMessages := make([][]byte, 0, len(commit.Signatures))
+	commitSignatures := make([][]byte, 0, len(commit.Signatures))
 	for idx, commitSig := range commit.Signatures {
 		if commitSig.Absent() {
 			continue // OK, some signatures can be absent.
@@ -689,19 +696,39 @@ func (vals *ValidatorSet) VerifyCommit(chainID string, blockID BlockID,
 		// This means we don't need the validator address or to do any lookup.
 		val := vals.Validators[idx]
 
-		// Validate signature.
-		voteSignBytes := commit.VoteSignBytes(chainID, int32(idx))
-		if !val.PubKey.VerifyBytes(voteSignBytes, commitSig.Signature) {
+		validatorPublicKeys = append(validatorPublicKeys, val.PubKey)
+		commitMessages = append(commitMessages, commit.VoteSignBytes(chainID, int32(idx)))
+		commitSignatures = append(commitSignatures, commitSig.Signature)
+	}
+
+	// Validate all of the signatures.
+	validSigs, err := batchsig.VerifyBatch(validatorPublicKeys, commitMessages, commitSignatures)
+	if err != nil {
+		return err
+	}
+
+	// Accumulate the voting power based on the valid signatures.
+	var sigIdx int
+	for idx, commitSig := range commit.Signatures {
+		if commitSig.Absent() {
+			continue // OK, some signatures can be absent.
+		}
+		if !validSigs[sigIdx] {
 			return fmt.Errorf("wrong signature (#%d): %X", idx, commitSig.Signature)
 		}
 		// Good!
 		if commitSig.ForBlock() {
+			// The vals and commit have a 1-to-1 correspondance.
+			// This means we don't need the validator address or to do any lookup.
+			val := vals.Validators[idx]
 			talliedVotingPower += val.VotingPower
 		}
 		// else {
 		// It's OK. We include stray signatures (~votes for nil) to measure
 		// validator availability.
 		// }
+
+		sigIdx++
 	}
 
 	if got, needed := talliedVotingPower, votingPowerNeeded; got <= needed {

oasisprotocol/cometbft

crypto/batchsig/batchsig.go
@@ -1,0 +1,44 @@
+// Package batchsig provides an abstraction for batch verifying public
+// key signatures.
+package batchsig
+
+import (
+	"fmt"
+
+	"github.com/oasisprotocol/ed25519"
+
+	"github.com/cometbft/cometbft/crypto"
+	tmEd25519 "github.com/cometbft/cometbft/crypto/ed25519"
+)
+
+var defaultOptions ed25519.Options
+
+// VerifyBatch verifies signatures in bulk.  Note that this call is only
+// faster than calling VerifyBytes for each signature iff every signature
+// is valid.
+func VerifyBatch(pubKeys []crypto.PubKey, msgs, sigs [][]byte) ([]bool, error) {
+	if len(pubKeys) != len(msgs) || len(msgs) != len(sigs) {
+		return nil, fmt.Errorf("tendermint/crypto/ed25519: parameter size mismatch")
+	}
+
+	// Currently only Ed25519 supports batch verification.
+	nativePubKeys := make([]ed25519.PublicKey, 0, len(pubKeys))
+	for i := range pubKeys {
+		edPubKey, ok := pubKeys[i].(tmEd25519.PubKey)
+		if !ok {
+			return verifyBatchFallback(pubKeys, msgs, sigs)
+		}
+
+		nativePubKeys = append(nativePubKeys, ed25519.PublicKey(edPubKey[:]))
+	}
+	_, validSigs, err := ed25519.VerifyBatch(nil, nativePubKeys, msgs, sigs, &defaultOptions)
+	return validSigs, err
+}
+
+func verifyBatchFallback(pubKeys []crypto.PubKey, msgs, sigs [][]byte) ([]bool, error) {
+	validSigs := make([]bool, len(pubKeys))
+	for i := range pubKeys {
+		validSigs[i] = pubKeys[i].VerifySignature(msgs[i], sigs[i])
+	}
+	return validSigs, nil
+}

types/validator_set.go
@@ -10,6 +10,8 @@ import (
 	"sort"
 	"strings"
 
+	"github.com/cometbft/cometbft/crypto"
+	"github.com/cometbft/cometbft/crypto/batchsig"
 	"github.com/cometbft/cometbft/crypto/merkle"
 	"github.com/cometbft/cometbft/crypto/tmhash"
 	cmtmath "github.com/cometbft/cometbft/libs/math"
@@ -712,6 +714,11 @@ func (vals *ValidatorSet) VerifyCommit(chainID string, blockID BlockID,
 
 	talliedVotingPower := int64(0)
 	votingPowerNeeded := vals.TotalVotingPower() * 2 / 3
+
+	// Aggregate all of the validator public key/messages/signatures.
+	validatorPublicKeys := make([]crypto.PubKey, 0, len(commit.Signatures))
+	commitMessages := make([][]byte, 0, len(commit.Signatures))
+	commitSignatures := make([][]byte, 0, len(commit.Signatures))
 	for idx, commitSig := range commit.Signatures {
 		if commitSig.Absent() {
 			continue // OK, some signatures can be absent.
@@ -721,19 +728,39 @@ func (vals *ValidatorSet) VerifyCommit(chainID string, blockID BlockID,
 		// This means we don't need the validator address or to do any lookup.
 		val := vals.Validators[idx]
 
-		// Validate signature.
-		voteSignBytes := commit.VoteSignBytes(chainID, int32(idx))
-		if !val.PubKey.VerifySignature(voteSignBytes, commitSig.Signature) {
+		validatorPublicKeys = append(validatorPublicKeys, val.PubKey)
+		commitMessages = append(commitMessages, commit.VoteSignBytes(chainID, int32(idx)))
+		commitSignatures = append(commitSignatures, commitSig.Signature)
+	}
+
+	// Validate all of the signatures.
+	validSigs, err := batchsig.VerifyBatch(validatorPublicKeys, commitMessages, commitSignatures)
+	if err != nil {
+		return err
+	}
+
+	// Accumulate the voting power based on the valid signatures.
+	var sigIdx int
+	for idx, commitSig := range commit.Signatures {
+		if commitSig.Absent() {
+			continue // OK, some signatures can be absent.
+		}
+		if !validSigs[sigIdx] {
 			return fmt.Errorf("wrong signature (#%d): %X", idx, commitSig.Signature)
 		}
 		// Good!
 		if commitSig.ForBlock() {
+			// The vals and commit have a 1-to-1 correspondance.
+			// This means we don't need the validator address or to do any lookup.
+			val := vals.Validators[idx]
 			talliedVotingPower += val.VotingPower
 		}
 		// else {
 		// It's OK. We include stray signatures (~votes for nil) to measure
 		// validator availability.
 		// }
+
+		sigIdx++
 	}
 
 	if got, needed := talliedVotingPower, votingPowerNeeded; got <= needed {